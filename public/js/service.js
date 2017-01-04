335075e54d8d882f35ab42621f5b5aba9451fa54
firdous alam <firdous.alam2058@gmail.com>
2017-01-04 17:01:22 +0530

    changes the post method

--- public/js/service.js
+++ public/js/service.js
@@ -2,21 +2,23 @@ angular.module('applicationService.services', [])
     .factory('API', function ($http) 
     {
 	  	//var base = "http://localhost:3500/";
-           var base = "http://139.59.11.3:3500/";
+           var base = "http://35.161.211.221:3500/";
         return  {
 				getDetails : function (url,params) {
 					return $http.get(base+url,
                     {
-                        method : 'GET',  
+                        method : 'GET',
+                        crossDomain: true,
                         headers:{'Content-Type': 'application/json'},
                         params: params
                     });
 				},
 				postDetails : function (form,url) {
 					return $http.post(base+url,
-                    form,
+                    JSON.stringify(form),
                     {
-                        method : 'POST',  
+                        method : 'POST',
+                        crossDomain: true, 
                         headers:{'Content-Type': 'application/json'}
                     });
 				},
