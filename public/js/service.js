ad2395e0b42f23b76efb5f2ed0a902c43bee6a00
firdous alam <firdous.alam2058@gmail.com>
2017-07-01 17:25:47 +0530

    change local url to live

--- public/js/service.js
+++ public/js/service.js
@@ -2,8 +2,8 @@ angular.module('applicationService.services', [])
     .factory('API', function ($http) 
     {
     //base location of the server     
-	 var base = "http://localhost:3500/";
-     //   var base = "http://ec2-35-161-211-221.us-west-2.compute.amazonaws.com:3500/";
+	// var base = "http://localhost:3500/";
+        var base = "http://ec2-35-161-211-221.us-west-2.compute.amazonaws.com:3500/";
         return  {
     				getDetails : function (url,params) {
     					return $http.get(base+url,
