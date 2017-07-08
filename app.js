dbcb10a21518e25976b2a54adbea63e76b514aa1
firdous alam <firdous.alam2058@gmail.com>
2017-07-08 17:58:58 +0530

    changing the port no for download pdf

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-server.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+server.listen(5454, function () {
+  console.log('Example app listening on port 5454!');
 });
 
 var users = {};
